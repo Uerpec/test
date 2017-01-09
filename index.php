171933d7ad6a8f54d81425e74452ae885fbe906e
Sergey Sergeevich <uerpec@gmail.com>
2017-01-09 17:23:44 +0300

    чет работает

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 require_once 'dataBase.php'; //класс подключения к базе
 require_once 'config.php'; //параметры подключения к базе
 
-
+//
 $dataBase = new DataBase($host,$user,$password,$base);
 $dataBase->connection_info();
