d8ef888f3701fc24f6fb9044fa302c55e4001ae8
Sergey Sergeevich <uerpec@gmail.com>
2017-01-09 17:22:50 +0300

    чет работает

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+//параменты подключения к базе
+$host='test.local';
+$user='root';
+$password='';
+$base= 'test';
--- function.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Uerpec
- * Date: 28.12.2016
- * Time: 19:51
- */
